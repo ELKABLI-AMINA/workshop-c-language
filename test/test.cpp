fb1dd8fe9addfc8ea0572fd8284a935d15d05414
faizamina <112840142+faizamina@users.noreply.github.com>
2022-09-07 17:22:30 +0100

    Add files via upload

---
+++ test/test.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+/*	int age, num;
+	char nom[10], prenom[20], sexe[10];
+	/*
+	printf("veuillez entrer votre nom");
+	scanf("%s",&nom);
+	printf("veuillez entrer votre prenom");
+	scanf("%s",&prenom);
+	printf("veuillez entrer votre sexe");
+	scanf("%s",&sexe);
+	printf("veuillez entrer votre age");
+	scanf("%d",&age);
+	printf("veuillez entrer votre num");
+	scanf("%d",&num);
+	
+	// exercice 2
+	float Fahrenheit, Celsius;
+	printf("veuillez entrer la temperature en Fahrenheit");
+	scanf("%f",&Fahrenheit);
+	Celsius = (Fahrenheit - 32 ) /  1.8;
+ 	printf("%.2f Fahrenheit= %.2f Celsius", Fahrenheit , Celsius );	 
+	
+	//  exercice 3
+	 float metre,mile,km;
+	 
+	 printf("veuillez entrer la distance en metre");
+	 scanf("%f",&metre);
+	 mile = km*1.609;
+     km = mile/1.609;
+	 metre =mile/1609;
+	 printf("%.3f metre = %.3f mile ",mile, metre );
+	 
+	 // exercice 4
+	 float mile, metre, km;
+	 printf("veuillez entrer la distance en mile");
+	 scanf("%f",&mile);
+	 km = mile/1.609;
+	
+	 metre =mile/1609;
+	 printf("%.3f mile = %.3f metre", mile, metre);
+	 
+	 
+	 // exercice 6
+	 float F,C;
+	 printf("veuillez enter la temperature F");
+	 scanf("%f",&F);
+	 C = (F-32)/1.8;
+	 printf(" la temperature est:%f",C);
+	 */
+	 // exercice 7
+	 
+	 int a, b;
+	 int somme,soustraction,multiplication;
+	 float division, reste;
+	 printf("veuillez entrer la valeur de a\n ");
+	 scanf("%d",&a);
+	 printf("veuillez entrer la valeur de b\n ");
+	 scanf("%d",&b);
+	 somme= a+b;
+	 soustraction=a-b;
+	 multiplication = a*b;
+	 division = a/b;
+	 reste=a%b;
+	 printf("somme de a et b:%d\n",somme);
+	 printf("soustraction de a et b :%d\n", soustraction);
+	 printf("multiplication de a et b:%d\n",multiplication);
+     printf("division de a et b:%f\n",division);
+	printf("reste de la division de a et b:%f",reste);
+	 
+	 
+ 
+ 	
+
+	 
+	  
+}
+
