1b32233a2958a588608eca5da945a74fe03698e9
amina <amina.elkabli1@gmail.com>
2022-09-08 14:03:19 +0100

    tous les challenges

---
+++ Untitled1.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include<math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+/*	int age, num;
+	char nom[10], prenom[20], sexe[10];
+	/*
+	printf("veuillez entrer votre nom");
+	scanf("%s",&nom);
+	printf("veuillez entrer votre prenom");
+	scanf("%s",&prenom);
+	printf("veuillez entrer votre sexe");
+	scanf("%s",&sexe);
+	printf("veuillez entrer votre age");
+	scanf("%d",&age);
+	printf("veuillez entrer votre num");
+	scanf("%d",&num);
+	
+	// exercice 2
+	float Fahrenheit, Celsius;
+	printf("veuillez entrer la temperature en Fahrenheit");
+	scanf("%f",&Fahrenheit);
+	Celsius = (Fahrenheit - 32 ) /  1.8;
+ 	printf("%.2f Fahrenheit= %.2f Celsius", Fahrenheit , Celsius );	 
+	
+	//  exercice 3
+	 float metre,mile,km;
+	 
+	 printf("veuillez entrer la distance en metre");
+	 scanf("%f",&metre);
+	 mile = km*1.609;
+     km = mile/1.609;
+	 metre =mile/1609;
+	 printf("%.3f metre = %.3f mile ",mile, metre );
+	 
+	 // exercice 4
+	 float mile, metre, km;
+	 printf("veuillez entrer la distance en mile");
+	 scanf("%f",&mile);
+	 km = mile/1.609;
+	
+	 metre =mile/1609;
+	 printf("%.3f mile = %.3f metre", mile, metre);
+	 
+	 
+	 // exercice 6
+	 float F,C;
+	 printf("veuillez enter la temperature F");
+	 scanf("%f",&F);
+	 C = (F-32)/1.8;
+	 printf(" la temperature est:%f",C);
+	 
+	 // exercice 7
+	 
+	 int a, b;
+	 int somme,soustraction,multiplication;
+	 float division, reste;
+	 printf("veuillez entrer la valeur de a\n ");
+	 scanf("%d",&a);
+	 printf("veuillez entrer la valeur de b\n ");
+	 scanf("%d",&b);
+	 somme= a+b;
+	 soustraction=a-b;
+	 multiplication = a*b;
+	 division = a/b;
+	 reste=a%b;
+	 printf("somme de a et b:%d\n",somme);
+	 printf("soustraction de a et b :%d\n", soustraction);
+	 printf("multiplication de a et b:%d\n",multiplication);
+     printf("division de a et b:%f\n",division);
+	printf("reste de la division de a et b:%f",reste);
+	 
+	 // exercice 8
+	 float N1, N2, N3, N4;
+	 float somme, moyenne;
+	 printf("veuillez entrer N1 ");
+	 scanf("%f",&N1);
+     printf("veuillez entrer N2");
+	 scanf("%f",&N2);
+	 printf("veuillez entrer N3 ");
+	 scanf("%f",&N3);
+	 printf("veuillez entrer N4 ");
+	 scanf("%f",&N4);
+	 somme = N1+N2+N3+N4;
+	 moyenne = somme/4;
+	 printf("la somme de N1 et N2 et N3 et N4 est : %f\n", somme);
+	 printf(" la moyenne de N1 et N2 et N3 et N4 est : %f", moyenne);
+	 
+	 // exercice 9
+	 
+	  float x1, x2, y1, y2, AB;
+	  
+	  printf("veuillez entrer la de x1 de premier point");
+	  scanf("%f",&x1);
+	  printf("veuillez entrer la de x2");
+	  scanf("%f",&x2);
+ 	  printf("veuillez entrer la de y1");
+	  scanf("%f",&y1);
+      printf("veuillez entrer la de y2");
+	  scanf("%f",&y2);
+	  (AB)= sqrt(pow((x2-x1),2) + pow ((y2-y1),2)) ;
+	  printf("la distance entre A et B est:%f",AB);
+	  
+	  // exercice 10
+	  float circonference, rayon;
+	  const double p=3.14;
+	  printf("donner le rayon du cercle");
+	  scanf("%f",&rayon);
+	  circonference= 2*p*rayon;
+	  printf("la circonference du rayon est:%f", circonference);
+	   
+	   // exercice 11
+	   float  circonference, longueur, largeur;
+	   printf("veuillez entrer la langueur");
+	   scanf("%f",&longueur);
+	   printf("veuillez entrer le largeur");
+	   scanf("%f",&largeur);
+	   circonference = 2*(longueur + largeur);
+	   printf("la circonference du rayon est:%f", circonference);
+	   */
+	   //exercice 12
+	   int a,b,c;
+	   printf("veuillez entrer la valeur de a : ");
+	   scanf("%d",&a);
+	   printf("veuillez entrer la valeur de b : ");
+	   scanf("%d",&b);
+	   printf("veuillez entrer la valeur de c : ");
+	   scanf("%d",&c);
+	   
+	   a=c;
+	   b=b;
+	   c=a;
+	   
+	   printf("l'inverse de a b c est :%d%d%d",c ,b, a);
+	   
+}
+
